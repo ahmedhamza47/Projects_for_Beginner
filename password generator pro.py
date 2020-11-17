bedf9c73fbb55148d42f7d2853972359e4bae784
ahmedhamza47 <muhammadhamza12342@gmail.com>
2020-11-17 19:09:40 +0545

    completed

--- password generator pro.py
+++ password generator pro.py
@@ -1,3 +1,4 @@
+import random as rd
 import secrets
 import string
 
@@ -8,6 +9,21 @@ def password_gen(pass_length):
     password += secrets.choice(string.ascii_uppercase)
     password += secrets.choice(string.digits)
     password += secrets.choice(string.punctuation)
-    print(password)
 
-password_gen(5)
+
+    for i in range(0, pass_length-4):
+        password += secrets.choice(pass_source)
+
+    pass_list = list(password)
+    secrets.SystemRandom().choice(pass_list)
+    password = ''.join(pass_list)
+    return password
+
+
+
+length = int(input('How long would you like your password to be:'))
+# digit = int(input('Enter the number of digits you would like:'))
+# alph = input('Enter the number of alphabet:')
+
+pas = password_gen(length)
+print(pas)
